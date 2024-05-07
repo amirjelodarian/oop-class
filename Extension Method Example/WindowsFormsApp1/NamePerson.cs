75dd2af1c0fffa8371aec8c7e41e493a8ce71cc9
amir__jn <52361065+amirjelodarian@users.noreply.github.com>
2024-05-08 02:12:19 +0330

    sesson 4
    sesson 4

---
+++ Extension Method Example/WindowsFormsApp1/NamePerson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public static class NamePerson
+    {
+        public static string GetFullName(this Person person)
+        {
+            return $"{person.Name} - {person.Family}";
+        }
+    }
+}
---
+++ Extension Method Example/WindowsFormsApp1/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Person
+    {
+        
+        public Person(int _age)
+        {
+            age = _age;            
+        }
+        public Person(int age, string name) : this(age)
+        {
+            this.age = age;
+            this.Name = name;
+        }
+        public Person() : this(5)
+        {
+            
+        }
+        public int Id { get; set; }
+        public int age { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+    }
+}
